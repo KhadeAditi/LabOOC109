1c353311bc09db736ddaea3bdf5215e4dcd429ce
KhadeAditi <aditikhade0@gmail.com>
2026-08-12 04:31:35 +0000

    class

---
+++ second.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,sum;
+    cout<<"enter two numbers for addition";
+    
+    cout<<"enter the first number";
+    cin>>a;
+
+    cout<<"enter the second number";
+    cin>>b;
+    
+}
